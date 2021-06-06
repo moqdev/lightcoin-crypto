b2213d1f2a434853a248b8e8c9548b20ca498d80
Mona Waseem <monaqwaseem@gmail.com>
2021-06-06 20:07:09 +0000

    Define a getter method called value in each subclass.

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-let balance = 500.00;
-
 class Account {
 
   constructor(username) {
     this.username = username;
-    // Have the account balance start at $0 since that makes more sense.
-    this.balance = 0;
+    this.transactions = [];
+  }
+
+  get balance() {
+  	let balance = 0;
+    for (let t of this.transactions) {
+    	balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
-class Transaction {
 
-  constructor(amount, account) {
-    this.amount  = amount;
-    this.account = account;
+get balance() {
+  	let balance = 0;
+    for (let t of this.transactions) {
+    	balance += t.value;
+    }
+    return balance;
   }
-
-}
-
-commit() {
-  this.account.balance += this.value;
-}
-
-}
-
-class Deposit extends Transaction {
-
-get value() {
-  return this.amount
-}
-
-}
-
-class Withdrawal extends Transaction {
-
-get value() {
-  return -this.amount;
-}
-
-}
 
 // DRIVER CODE BELOW
 
-const myAccount = new Account('billybob');
+const myAccount = new Account();
 
-console.log('Starting Balance:', myAccount.balance);
+console.log('Starting Account Balance: ', myAccount.balance);
 
-const t1 = new Deposit(120.00, myAccount);
-t1.commit();
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-const t2 = new Withdrawal(50.00, myAccount);
-t2.commit();
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-console.log('Ending Balance:', myAccount.balance);
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
+
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
 
 
 
