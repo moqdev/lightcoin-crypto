fe2d8bb6674877359b459d6ceeca03f4cb0b9ea5
Mona Waseem <monaqwaseem@gmail.com>
2021-06-06 19:25:52 +0000

    Create Account class and update Withdrawal and Deposit classes to use new account object.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 let balance = 500.00;
 
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+
+}
+
+class Deposit {
+
+  // Pass in the account that the deposit this for
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  // Update the balance in the account
+  commit() {
+    this.account.balance += this.amount;
+  }
+
+}
+
 class Withdrawal {
 
   constructor(amount) {
@@ -17,6 +42,11 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
+
+t3 = new Deposit(120.00);
+t3.commit();
+console.log('Transaction 3:', t3);
 
 t1 = new Withdrawal(50.25);
 t1.commit();
