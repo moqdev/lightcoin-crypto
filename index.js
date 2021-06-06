5e53a398748f16998894f8cf8d87043659b909b9
Mona Waseem <monaqwaseem@gmail.com>
2021-06-06 19:42:20 +0000

    Refactor commit and update commit methods to use this.value and this.amount

--- index.js
+++ index.js
@@ -19,21 +19,41 @@ class Transaction {
 
 }
 
+commit() {
+  this.account.balance += this.value;
+}
+
+}
+
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
-  }
+get value() {
+  return this.amount
+}
 
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
-  }
+get value() {
+  return -this.amount;
+}
 
 }
+
+// DRIVER CODE BELOW
+
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
+t1.commit();
+
+const t2 = new Withdrawal(50.00, myAccount);
+t2.commit();
+
+console.log('Ending Balance:', myAccount.balance);
 
 
 
